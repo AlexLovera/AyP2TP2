6a54c2917b7da7894cc6a56d59491dffa310217e
Alexxlll <Alexander.lovera8@hotmail.com>
2019-06-02 16:40:38 -0300

    Agrego clase MinaYProvision, con su ejecutar correspondiente

---
+++ src/app/MinaYProvision.java
@@ -0,0 +1,24 @@
+package app;
+
+public class MinaYProvision implements ObjetosEscondidos{
+	private Mina mina;
+	private Provisiones provisiones;
+
+	public MinaYProvision(Mina mina,Provisiones provisiones) {
+		this.mina=mina;
+		this.provisiones=provisiones;
+	}
+	
+	//primero se agrega la provision y luego explota la bomba
+	@Override
+	public void ejecutarComportamiento(Pac pac) {
+		provisiones.ejecutarComportamiento(pac);
+		mina.ejecutarComportamiento(pac);
+	}
+
+	@Override
+	public boolean sePuedeAvanzar() {
+		return true;
+	}
+	
+}
