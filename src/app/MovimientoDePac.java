aafcdf9cddb23c9e9eed25ae4340a919998f5516
Alexxlll <Alexander.lovera8@hotmail.com>
2019-06-02 16:57:49 -0300

    Se agregaron metodos para las direcciones faltantes

--- src/app/MovimientoDePac.java
+++ src/app/MovimientoDePac.java
@@ -3,7 +3,18 @@ package app;
 public class MovimientoDePac {
 	VerificadorDePosicion verificador = new VerificadorDePosicion();
 	
-	public boolean moverAPacALaDerecha() {
+	public boolean moverAPacHaciaLaDerecha() {
+		int posicionActual = Pac.getPac().obtenerPosicionDePac() + 1;
+		if (Tablero.getTableroSingleton().devolverObjetoEnCasillero(posicionActual).sePuedeAvanzar()) {
+			Tablero.getTableroSingleton().devolverObjetoEnCasillero(posicionActual)
+					.ejecutarComportamiento(Pac.getPac());
+			Pac.getPac().moverAPac(posicionActual);
+			return true;
+		} else
+			return false;
+	}
+	
+	public boolean moverAPacHaciaLaIzquierda() {
 		int posicionActual = Pac.getPac().obtenerPosicionDePac() - 1;
 		if (Tablero.getTableroSingleton().devolverObjetoEnCasillero(posicionActual).sePuedeAvanzar()) {
 			Tablero.getTableroSingleton().devolverObjetoEnCasillero(posicionActual)
@@ -13,4 +24,27 @@ public class MovimientoDePac {
 		} else
 			return false;
 	}
+	
+	public boolean moverAPacHaciaAbajo() {
+		int posicionActual = Pac.getPac().obtenerPosicionDePac() + Tablero.getTableroSingleton().getNumeroDeColumnas();
+		if (Tablero.getTableroSingleton().devolverObjetoEnCasillero(posicionActual).sePuedeAvanzar()) {
+			Tablero.getTableroSingleton().devolverObjetoEnCasillero(posicionActual)
+					.ejecutarComportamiento(Pac.getPac());
+			Pac.getPac().moverAPac(posicionActual);
+			return true;
+		} else
+			return false;
+	}
+	
+	public boolean moverAPacHaciaArriba() {
+		int posicionActual = Pac.getPac().obtenerPosicionDePac() - Tablero.getTableroSingleton().getNumeroDeColumnas();
+		if (Tablero.getTableroSingleton().devolverObjetoEnCasillero(posicionActual).sePuedeAvanzar()) {
+			Tablero.getTableroSingleton().devolverObjetoEnCasillero(posicionActual)
+					.ejecutarComportamiento(Pac.getPac());
+			Pac.getPac().moverAPac(posicionActual);
+			return true;
+		} else
+			return false;
+	}
+	
 }
